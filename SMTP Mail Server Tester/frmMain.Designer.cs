dae2b122a9f6858137e4a6f57e2ecd1ca05b91a2
luca@lucamerega.it <standard0>
2018-02-06 20:32:07 +0100

    Add project files.

---
+++ SMTP Mail Server Tester/frmMain.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace SMTP_Mail_Server_Tester
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gb01 = new System.Windows.Forms.GroupBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.lblHostName = new System.Windows.Forms.Label();
+            this.btnStartTest = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.gb01.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gb01
+            // 
+            this.gb01.Controls.Add(this.txtPassword);
+            this.gb01.Controls.Add(this.lblPassword);
+            this.gb01.Controls.Add(this.txtUsername);
+            this.gb01.Controls.Add(this.lblUsername);
+            this.gb01.Controls.Add(this.txtPort);
+            this.gb01.Controls.Add(this.lblPort);
+            this.gb01.Controls.Add(this.txtHost);
+            this.gb01.Controls.Add(this.lblHostName);
+            this.gb01.Location = new System.Drawing.Point(12, 12);
+            this.gb01.Name = "gb01";
+            this.gb01.Size = new System.Drawing.Size(529, 85);
+            this.gb01.TabIndex = 12;
+            this.gb01.TabStop = false;
+            this.gb01.Text = "SMTP Parameters";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(331, 46);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(182, 20);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(269, 53);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 6;
+            this.lblPassword.Text = "Password:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(70, 49);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(193, 20);
+            this.txtUsername.TabIndex = 3;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(6, 56);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(58, 13);
+            this.lblUsername.TabIndex = 4;
+            this.lblUsername.Text = "Username:";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(331, 19);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(47, 20);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.Text = "25";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(269, 27);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(29, 13);
+            this.lblPort.TabIndex = 2;
+            this.lblPort.Text = "Port:";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(106, 19);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(157, 20);
+            this.txtHost.TabIndex = 0;
+            // 
+            // lblHostName
+            // 
+            this.lblHostName.AutoSize = true;
+            this.lblHostName.Location = new System.Drawing.Point(6, 27);
+            this.lblHostName.Name = "lblHostName";
+            this.lblHostName.Size = new System.Drawing.Size(94, 13);
+            this.lblHostName.TabIndex = 0;
+            this.lblHostName.Text = "&Host (Name or IP):";
+            // 
+            // btnStartTest
+            // 
+            this.btnStartTest.Location = new System.Drawing.Point(466, 118);
+            this.btnStartTest.Name = "btnStartTest";
+            this.btnStartTest.Size = new System.Drawing.Size(75, 23);
+            this.btnStartTest.TabIndex = 13;
+            this.btnStartTest.Text = "Start Test";
+            this.btnStartTest.UseVisualStyleBackColor = true;
+            this.btnStartTest.Click += new System.EventHandler(this.btnStartTest_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(21, 127);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(46, 13);
+            this.lblStatus.TabIndex = 14;
+            this.lblStatus.Text = "Status...";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 153);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnStartTest);
+            this.Controls.Add(this.gb01);
+            this.Name = "frmMain";
+            this.Text = "SMTP Mail Server Tester";
+            this.gb01.ResumeLayout(false);
+            this.gb01.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gb01;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Label lblHostName;
+        private System.Windows.Forms.Button btnStartTest;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ SMTP Mail Server Tester/frmMain.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.IO;
+
+namespace SMTP_Mail_Server_Tester
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        Exception ex;
+
+        private void btnStartTest_Click(object sender, EventArgs e)
+        {
+
+            if (txtHost.Text.Length < 5)
+            {
+                MessageBox.Show("Host not specified", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            ex = new Exception();
+            try
+            {
+                SmtpClient client = new SmtpClient();
+
+                if (txtHost.Text != "")
+                    client.Host = txtHost.Text;
+
+                if (txtPort.Text != "")
+                    client.Port = Convert.ToInt32(txtPort.Text);
+
+                if (txtUsername.Text != "" && txtPassword.Text != "")
+                {
+                    client.UseDefaultCredentials = false;
+                    System.Net.NetworkCredential credencials =
+                        new System.Net.NetworkCredential(txtUsername.Text, txtPassword.Text);
+                    client.Credentials = credencials;
+                }
+                else
+                    client.UseDefaultCredentials = true;
+
+                MailMessage mailMessage = new MailMessage();
+
+                mailMessage.From = new MailAddress("test@gmail.com");
+                mailMessage.To.Add("test@gmail.com");
+                mailMessage.Subject = "This is a test";
+                mailMessage.Body = "This is the bofy. Just a test";
+                client.Send(mailMessage);
+
+                lblStatus.Text = "Email successfully sent";
+            }
+            catch (Exception ex2)
+            {
+                ex = ex2;
+                lblStatus.Text = "Error Message: " + ex.Message + " || Erro Stack Trace: " + ex.StackTrace;
+            }
+
+        }
+    }
+}
